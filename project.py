6372ed8d58693877fbd2204129160c5957f539c5
0507jiho <32870397+0507jiho@users.noreply.github.com>
2023-12-03 23:17:33 +0900

    Recognize eyes and nose
    Draw circles at the positions of the eyes and nose

--- project.py
+++ project.py
@@ -7,7 +7,7 @@ mp_drawing = mp.solutions.drawing_utils
 # Load the video file
 cap = cv2.VideoCapture('interview.mp4')
 
-with mp_face_detection.FaceDetection(model_selection=0, min_detection_confidence=0.5) as face_detection:
+with mp_face_detection.FaceDetection(model_selection=0, min_detection_confidence=0.3) as face_detection:
     while cap.isOpened():
         success, image = cap.read()
         if not success:
@@ -23,8 +23,24 @@ with mp_face_detection.FaceDetection(model_selection=0, min_detection_confidence
         image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
         if results.detections:
+            # 6 characteristics: right eye, left eye, tip of the nose, center of the mouth, right ear, left ear
             for detection in results.detections:
-                mp_drawing.draw_detection(image, detection)
+                # mp_drawing.draw_detection(image, detection)
+                
+                # Retrieve the position of a specific feature among the six characteristics.
+                keypoints = detection.location_data.relative_keypoints
+                right_eye = keypoints[0]
+                left_eye = keypoints[1]
+                nose_tip = keypoints[2]
+
+                h, w, _ = image.shape
+                right_eye = (int(right_eye.x * w), int(right_eye.y * h))
+                left_eye = (int(left_eye.x * w), int(left_eye.y * h))
+                nose_tip = (int(nose_tip.x * w), int(nose_tip.y * h))
+
+                cv2.circle(image, right_eye, 20, (255, 0, 0), 10, cv2.LINE_AA)
+                cv2.circle(image, left_eye, 20, (0, 255, 0), 10, cv2.LINE_AA)
+                cv2.circle(image, nose_tip, 40, (0, 255, 255), 10, cv2.LINE_AA)
 
         # Show the image with face detection
         cv2.imshow('MediaPipe Face Detection', cv2.resize(image, None, fx = 0.5, fy = 0.5))

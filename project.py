dc15a922a6522c0ed3ee9be3b80430416af1bfa6
Josh <144087167+kirasjun@users.noreply.github.com>
2023-12-14 16:38:17 +0900

    Update project.py
    Finding eyes and nose in video and pixeling them

--- project.py
+++ project.py
@@ -2,53 +2,49 @@ import cv2
 import mediapipe as mp
 
 mp_face_detection = mp.solutions.face_detection
-mp_drawing = mp.solutions.drawing_utils
+
+image_right_eye = cv2.imread('eye.png')
+image_right_eye = cv2.resize(image_right_eye, (100, 63))
+
+image_left_eye = cv2.imread('eye.png')
+image_left_eye = cv2.resize(image_left_eye, (100, 63))
 
 # Load the video file
 cap = cv2.VideoCapture('foreigner_interview.mp4')
 
-with mp_face_detection.FaceDetection(model_selection=0, min_detection_confidence=0.3) as face_detection:
+with mp_face_detection.FaceDetection(model_selection=0, min_detection_confidence=0.7) as face_detection:
     while cap.isOpened():
         success, image = cap.read()
         if not success:
             break
-
+        
         # Convert the image to RGB for Mediapipe processing
-        image.flags.writeable = False
         image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        results = face_detection.process(image)
-
-
-        image.flags.writeable = True
-        image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        results = face_detection.process(image_rgb)
 
         if results.detections:
-            # 6 characteristics: right eye, left eye, tip of the nose, center of the mouth, right ear, left ear
             for detection in results.detections:
                 # mp_drawing.draw_detection(image, detection)
                 
-                # Retrieve the position of a specific feature among the six characteristics.
+                # Retrieve the position of a specific feature among the 2 characteristics.
                 keypoints = detection.location_data.relative_keypoints
                 right_eye = keypoints[0]
                 left_eye = keypoints[1]
-                nose_tip = keypoints[2]
 
                 h, w, _ = image.shape
-                right_eye = (int(right_eye.x * w), int(right_eye.y * h))
-                left_eye = (int(left_eye.x * w), int(left_eye.y * h))
-                nose_tip = (int(nose_tip.x * w), int(nose_tip.y * h))
+                right_eye_pos = (int(right_eye.x * w)), (int(right_eye.y * h))
+                left_eye_pos = (int(left_eye.x * w)), (int(left_eye.y * h))
 
-                cv2.circle(image, right_eye, 20, (255, 0, 0), 10, cv2.LINE_AA)
-                cv2.circle(image, left_eye, 20, (0, 255, 0), 10, cv2.LINE_AA)
-                cv2.circle(image, nose_tip, 40, (0, 255, 255), 10, cv2.LINE_AA)
+                image[right_eye_pos[1]-31:right_eye_pos[1]+32, right_eye_pos[0]-50:right_eye_pos[0]+50] = image_right_eye
+                image[left_eye_pos[1]-31:left_eye_pos[1]+32, left_eye_pos[0]-50:left_eye_pos[0]+50] = image_left_eye
 
         # Show the image with face detection
-        cv2.imshow('MediaPipe Face Detection', cv2.resize(image, None, fx = 0.5, fy = 0.5))
+        cv2.imshow('Face Detection with Eyes', image)
 
-        # Check for 'a' key press to exit the loop
+         # Check for 'a' key press to exit the loop
         if cv2.waitKey(1) == ord('q'):
             break
 
-# Release the video capture and destroy any open CV windows
-cap.release()
-cv2.destroyAllWindows()
+    # Release the video capture and destroy any open CV windows
+    cap.release()
+    cv2.destroyAllWindows() 
